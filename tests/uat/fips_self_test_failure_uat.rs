650bed43be5184436aa0ed0db13d50be0b82c579
Kirky-X <kirky-x@outlook.com>
2025-12-22 17:31:41 +0800

    chore: cleanup deleted files and remaining changes

--- tests/uat/fips_self_test_failure_uat.rs
+++ tests/uat/fips_self_test_failure_uat.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2025 Kirky.X
+// 
+// Licensed under the MIT License
+// See LICENSE file in the project root for full license information.
+
 #[cfg(feature = "fips")]
 #[test]
 fn test_fips_self_test_failure_handling() {
