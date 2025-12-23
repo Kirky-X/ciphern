7c7d893c90c14641f64cc377c3ca026402c3dc13
Kirky-X <kirky-x@outlook.com>
2025-12-23 15:01:06 +0800

    docs: add copyright headers to FFI source and test files
    Added MIT license copyright headers to FFI implementation and related test files:
    - src/ffi/python_pyo3.rs
    - tests/ffi/JavaTest.java
    - tests/ffi/c_ffi_test.c
    - tests/ffi/python_test.py (using # comments)
    
    Consistent with project-wide licensing standards.

--- src/ffi/python_pyo3.rs
+++ src/ffi/python_pyo3.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2025 Kirky.X
+// 
+// Licensed under the MIT License
+// See LICENSE file in the project root for full license information.
+
 #![allow(non_local_definitions)]
 #[cfg(feature = "encrypt")]
 use crate::Cipher;
